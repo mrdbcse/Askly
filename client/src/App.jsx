e6e368a60517c77b96af0538887502c849fddf61
Debjyoti Banerjee <mr.db.cse@gmail.com>
2025-12-27 23:26:30 +0530

    WIP-UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+import Chat from "./components/Chat";
 import Sidebar from "./components/Sidebar";
+import Community from "./pages/Community";
+import Credits from "./pages/Credits";
 
 const App = () => {
   return (
     <>
-      <div>
-        <Sidebar />
+      <div className="dark:bg-linear-to-b from-[#242124] to-[#000000] dark:text-white">
+        <div className="flex h-screen w-screen">
+          <Sidebar />
+          <Routes>
+            <Route path="/" element={<Chat />} />
+            <Route path="/credits" element={<Credits />} />
+            <Route path="/community" element={<Community />} />
+          </Routes>
+        </div>
       </div>
     </>
   );
--- client/src/components/Chatbox.jsx
+++
@@ -1,5 +0,0 @@
-const Chatbox = () => {
-  return <div>Chatbox</div>;
-};
-
-export default Chatbox;
